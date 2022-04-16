6cc391825ffb8dcf0d29f8d3fe2133d7eaca0257
Trinity Gao <ngao@ucsd.edu>
2022-04-15 20:55:46 -0700

    add

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -12,10 +12,9 @@ public class MarkdownParse {
         // find the next [, then find the ], then find the (, then read link upto next )
         int currentIndex = 0;
         while(currentIndex < markdown.length()) {
-            // if (can't find open paren and close paren) {
-                //break;
-
-            //}
+            if (markdown.indexOf("(", currentIndex) == -1) {
+                break;
+            }
             int openBracket = markdown.indexOf("[", currentIndex);
             int closeBracket = markdown.indexOf("]", openBracket);
             int openParen = markdown.indexOf("(", closeBracket);
