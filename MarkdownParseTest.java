51dc1f222b77ca3183f7579933d073e33df5693e
Trinity Gao <ngao@ucsd.edu>
2022-04-21 10:37:35 -0700

    Create MarkdownParseTest.java

---
+++ MarkdownParseTest.java
@@ -0,0 +1,11 @@
+import static org.junit.Assert.*;
+import org.junit.*;
+
+public class MarkdownParseTest {
+    @Test
+    public void addition() { // method with no return, only for junit test
+        
+        // if 1+1 = 2, return true. assertEquals(expected, actual);
+        assertEquals(2, 1 + 1);
+    }
+}
